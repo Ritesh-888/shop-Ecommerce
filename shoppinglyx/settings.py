14ccc6e552ce262915d13861844718a88eaf5c29
ritesh kumar chaurasia <riteshkumarchaurasia08@gmail.com>
2024-12-12 00:46:40 +0530

    commit

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_REDIRECT_URL = '/profile/'
